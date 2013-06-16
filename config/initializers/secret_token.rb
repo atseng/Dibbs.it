cc4c83439cf44c5737e52205ef506aee533a39d3
Allen <atseng247@gmail.com>
2013-06-15 20:14:12 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dibbs::Application.config.secret_token = 'a145341fac77f8f877b86c9f34920e1055bc066dd3ac443b0da5cb0e9ac7c8c26e5a51eb1d438f6a09d884f5813cda342969c31d953478d139e8c98c84a6a956'
---
+++ spec/features/user_views_welcome_page_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe 'a user visting the welcome page' do %q{
+
+  As a Visitor
+  I want to be able to sign up
+  So I can begin using Dibbs
+
+  AC
+  * Visitor can sign up
+  * Visitor must provide a name
+  * Visitor must provide an email
+  * Visitor must provide a password
+  * Visitor must confirm password
+
+}
+
+  describe 'visitor must sign in to use app' do
+    it 'displays sign in as the welcome page' do
+      visit users_path
+      expect(page).to have_content('Sign in')
+    end
+  end

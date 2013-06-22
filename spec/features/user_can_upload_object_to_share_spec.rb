714e10e9dad9d79576721c5002a806267e3fedb7
Allen <atseng247@gmail.com>
2013-06-21 21:34:31 -0400

    create upload item tests

--- spec/features/user_can_upload_object_to_share_spec.rb
+++ spec/features/user_can_upload_object_to_share_spec.rb
@@ -14,5 +14,31 @@ describe 'User can upload an object' do %q{
   * I can specify object value
 
 }
+  describe 'Signed in User submits valid object' do
+  let(:user) {FactoryGirl.create(:user)}
+    it 'creates a valid item ' do
+      sign_in(user)
+      item_count = Item.count
+      visit new_item_path
+      fill_in "Name", with: "SFX Lawnmower"
+      fill_in "Description", with: "Steel blade high power. Stiff shifter."
+      fill_in "Value", with: 120
+      click_link "Add Item"
+      expect(page).to have_content("Item created")
+      expect(item_count + 1).to eq(Item.count)
+    end
+  end
+
+  describe 'Signed in User submits invalid object' do
+    let(:user) {FactoryGirl.create(:user)}
+    it 'creates a invalid item ' do
+      sign_in(user)
+      item_count = Item.count
+      visit new_item_path
+      click_link "Add Item"
+      expect(page).to have_content("can't be blank")
+      expect(item_count).to eq(Item.count)
+    end
+  end
 
 end

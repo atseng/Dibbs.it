ac5cd4522ee18ac8aa731616a24b3ce94a4f7ebc
Allen <atseng247@gmail.com>
2013-06-24 16:12:19 -0400

    create friends controller

--- spec/helpers/friends_helper_spec.rb
+++ spec/helpers/friends_helper_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
 # Specs in this file have access to a helper object that includes
-# the FriendshipsHelper. For example:
+# the FriendsHelper. For example:
 #
-# describe FriendshipsHelper do
+# describe FriendsHelper do
 #   describe "string concat" do
 #     it "concats two strings with spaces" do
 #       helper.concat_strings("this","that").should == "this that"
 #     end
 #   end
 # end
-describe FriendshipsHelper do
+describe FriendsHelper do
   pending "add some examples to (or delete) #{__FILE__}"
 end

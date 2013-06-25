c900f038472ec70aaac8b2475472bb9f42efa8a6
Allen <atseng247@gmail.com>
2013-06-25 19:28:36 -0400

    create user inverse friends associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ActiveRecord::Base
 
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user, :conditions => "status = 'accepted'"
+  has_many :inverse_pending_friends, :through => :inverse_friendships, :source => :friend, :conditions => "status ='pending'"
 
   def all_friends
     @all_friends ||= friends + inverse_friends

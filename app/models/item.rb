e826676462b3c203a8a7b5a31faf8431582be6be
Allen <atseng247@gmail.com>
2013-07-01 13:52:14 -0400

    create item state methods

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,5 +11,23 @@ class Item < ActiveRecord::Base
   validates_presence_of :description
   validates_presence_of :value
 
+
+  def loaned?
+    loans.where(state: 'loaned').present?
+  end
+
+  def currently_loaned
+    loans.where(state: 'loaned')
+  end
+
+  def returned?
+    loans.where(state: 'returned').present?
+  end
+
+  def currently_returned
+    loans.where(state: 'returned')
+  end
+
+
   attr_accessible :avatar, :description, :name, :user_id, :value
 end

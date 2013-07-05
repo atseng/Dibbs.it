30aaee88e8240fb0206729deec2a17182638a8a7
Allen <atseng247@gmail.com>
2013-07-05 16:39:10 -0400

    user can view friend's profile test complete

--- spec/features/user_can_view_friends_spec.rb
+++ spec/features/user_can_view_friends_spec.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 describe 'User can view their friends' do %q{
 
   As a User
@@ -16,11 +18,24 @@ describe 'User can view their friends' do %q{
     let!(:friend2) { FactoryGirl.create(:user) }
     it "views a friend's profile page" do
       create_friendship(friend1, friend2)
-      visit user_friendships_path(friend)
-      expect(page).to have_content(friend.name)
+      visit user_friendships_path(friend1)
+      click_link friend1.name
+      expect(page).to have_content(friend1.name)
     end
   end
 
+  describe " a friend's profile" do
+    let!(:friend1) { FactoryGirl.create(:user) }
+    let!(:friend2) { FactoryGirl.create(:user) }
+    let!(:item) { FactoryGirl.create_list(:item, 3, user: friend1) }
+    it 'shows a list of available items' do
+      create_friendship(friend1, friend2)
+      visit user_friendships_path(friend1)
+      click_link friend1.name
+      save_and_open_page
+      expect(friend1.items.count).to eq(3)
+    end
+  end
 
 
 

c0156984360a24672f6d1ee09b21d1250369e730
Allen <atseng247@gmail.com>
2013-07-05 11:50:56 -0400

    complete user can search for friends test

--- spec/features/user_can_search_for_friends_spec.rb
+++ spec/features/user_can_search_for_friends_spec.rb
@@ -13,21 +13,31 @@ describe 'User can search for other users' do %q{
 }
 
   describe 'User searching for a friend' do
+    let!(:user3) {FactoryGirl.create(:user, :name => "Shredder") }
+    let!(:user2) {FactoryGirl.create(:user, :name => "Michaelango") }
+    let(:user) {FactoryGirl.create(:user) }
     it 'creates a search by name query' do
       sign_in(user)
-      visit user_index_path
+      visit users_path
       fill_in "User Name or Email", with: "Michaelango"
-
+      click_button "Search"
+      expect(page).to have_content(user2.name)
+      expect(page).to_not have_content(user3.name)
     end
   end
 
 
-    describe 'User searching for a friend' do
+  describe 'User searching for a friend' do
+    let!(:user3)  {FactoryGirl.create(:user, :email => "shredder@nyc.com") }
+    let!(:user2) {FactoryGirl.create(:user, :email => "leonardo@nyc.com") }
+    let(:user) {FactoryGirl.create(:user) }
     it 'creates a search by email query' do
       sign_in(user)
-      visit user_index_path
+      visit users_path
       fill_in "User Name or Email", with: "leonardo@nyc.com"
-
+      click_button "Search"
+      expect(page).to have_content(user2.email)
+      expect(page).to_not have_content(user3.email)
     end
   end
 end

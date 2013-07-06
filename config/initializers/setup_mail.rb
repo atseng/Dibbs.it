44fc6ecdc217fd1b06cf17b39eea027e675ca700
Allen <atseng247@gmail.com>
2013-07-06 18:02:57 -0400

    everything functions except json user can view their items

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,3 +1,5 @@
+require 'development_mail_interceptor'
+
 ActionMailer::Base.smtp_settings = {
   :address                    => "smtp.gmail.com",
   :port                       => 587,
@@ -7,6 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :authentication             => 'plain',
   :enable_starttls_auto       => true
 }
+
 
 ActionMailer::Base.default_url_options[:host] = "localhost:3000"
 ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?

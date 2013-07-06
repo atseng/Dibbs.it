2bf8db2e5b9dc143d9fcc123162259ce6e4591ca
Allen <atseng247@gmail.com>
2013-07-06 14:07:53 -0400

    basic setup invitations controller

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,9 +1,20 @@
 class InvitationsController < ApplicationController
+  before_filter :authenticate
 
   def new
+    @invitation = Invitation.new
   end
 
-
+  def create
+    @invitation = Invitation.new(params[:invitation])
+    @invitation.sender = current_user
+    if @invitation.save
+      flash[:notice] = "Thank you, friend invitation sent."
+      redirect_to edit_user_path(current_user)
+    else
+      render :action => 'new'
+    end
+  end
 
 
 
